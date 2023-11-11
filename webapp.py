7ab68aae6cbfba8430422ae988d6db349c7320ff
PolskieBadaniaCzytelnictwa <149326679+PolskieBadaniaCzytelnictwa@users.noreply.github.com>
2023-11-11 14:32:55 +0100

    Update plots

--- webapp.py
+++ webapp.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 os.chdir(os.path.dirname(__file__))
+
+def format_number_with_spaces(number_str):
+    # formatowanie liczb do formatu 1 111 111
+    reversed_str = str(int(number_str))[::-1]
+    groups = [reversed_str[i:i + 3] for i in range(0, len(reversed_str), 3)]
+    return ' '.join(groups)[::-1]
+
+
+my_colors = {
+    'Facebook': '#0070C0',
+    'YouTube': '#981923',
+    'X': '#193441',
+    'LinkedIn': '#006683',
+    'TikTok': '#00F0F0',
+    'Pinterest': '#E0404B',
+}
 
 
 # dane dot. followersów
@@ -29,7 +46,7 @@ else:
 
 
 media=list(df.columns.drop(['Suma', 'Typ']))
-st.write('Wybierz media społecznościowe: ')
+st.markdown('<p style="font-size: 14px;">Wybierz media społecznościowe:</p>', unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
     facebook = st.checkbox('Facebook', value=True)
@@ -51,13 +68,37 @@ medialist = [
 selected_columns = [x[1] for x in medialist if x[0]]
 filtered_df = filtered_df[selected_columns]
 
-# Przygotowanie i wyświetlenie tabeli
-filtered_df.index.name = None
-filtered_df = filtered_df.style.set_table_styles([
-    {'selector': 'table', 'props': [('text-align', 'center')]},
-    {'selector': 'th', 'props': [('text-align', 'center')]},
-    {'selector': 'td', 'props': [('text-align', 'center')]},
-{'selector': 'th.col0, td.col0', 'props': [('text-align', 'left')]}
-])
-filtered_df = filtered_df.to_html()
-st.markdown(f"<div style='height: 32.5em; overflow-x: scroll;'>{filtered_df}</div>", unsafe_allow_html=True)
+output_type = st.selectbox('Wybierz tryb wyświetlania danych:', ['Tabela', 'Wykresy'])
+
+if output_type=='Tabela':
+    # Przygotowanie i wyświetlenie tabeli
+    filtered_df.index.name = None
+    filtered_df = filtered_df.style.set_table_styles([
+        {'selector': 'table', 'props': [('text-align', 'center')]},
+        {'selector': 'th', 'props': [('text-align', 'center')]},
+        {'selector': 'td', 'props': [('text-align', 'center')]},
+    {'selector': 'th.col0, td.col0', 'props': [('text-align', 'left')]}
+    ])
+    filtered_df = filtered_df.to_html()
+    st.markdown(f"<div style='height: 32.5em; overflow-x: scroll;'>{filtered_df}</div>", unsafe_allow_html=True)
+else:
+    for column in selected_columns:
+        aux = filtered_df[filtered_df[column]!='-'][column].sort_values(ascending=False).head(10)
+        
+        fig, ax = plt.subplots(figsize=(8, 6))
+        plt.barh(aux.index, aux, color=my_colors[column], height=0.5)
+        plt.gca().spines[:].set_visible(False)
+        plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+        plt.tick_params(axis='y', which='both', length=0, labelleft=False) # labelleft=False żeby wykresy zaczynały się w tym samym miejscu
+        plt.gca().invert_yaxis()
+
+        plt.title(f'Top 10 pism: obserwujący w serwisie {column}',
+                   loc='left',
+                   fontdict={'fontsize': 14, 'fontweight': 'bold', 'fontname': 'Lato'})
+        for index, value in enumerate(list(aux)):
+            plt.text(value, index+.1, format_number_with_spaces(value))
+            
+        for index, pismo in enumerate(list(aux.index)):
+            plt.text(0, index-0.48, pismo, ha='left', va='center', fontdict={'fontsize': 10.8, 'fontname': 'Lato'})
+        st.pyplot(fig)
+
