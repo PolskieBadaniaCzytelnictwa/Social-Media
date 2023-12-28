9c0025af1497385ac327e7bad307e4e18de8a01f
PolskieBadaniaCzytelnictwa <149326679+PolskieBadaniaCzytelnictwa@users.noreply.github.com>
2023-12-28 23:04:21 +0100

    Setup file update

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ email = \"aleks.kapich@pbc.pl\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]\n\
+base = 'light'\n\
+primaryColor = '#00AADB'\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
