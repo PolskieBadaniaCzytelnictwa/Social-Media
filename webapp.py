dde0ed39bb3b7c63cafe73850af31ce027f36647
PolskieBadaniaCzytelnictwa <149326679+PolskieBadaniaCzytelnictwa@users.noreply.github.com>
2026-04-16 01:20:30 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -420,7 +420,7 @@ else:
 
         st.pyplot(barplot(df=aux, column=column))
 
-footer_message = "Źródło: Liczba obserwatorów w mediach społecznościowych, opracowanie własne PBC, dane na dzień 07.01.2026"
+footer_message = "Źródło: Liczba obserwatorów w mediach społecznościowych, opracowanie własne PBC, dane na dzień 14.04.2026"
 st.markdown(
     f"""<div style="font-size:12px">{footer_message}</div>""", unsafe_allow_html=True
 )
